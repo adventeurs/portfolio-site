bb1733c74db72dfd91082c624aa74c5a32db7736
Justin Miller <adventeurs@gmail.com>
2020-08-08 22:44:56 -0500

    create menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ProjectComponent } from "./projects/project.component";
+import { ResumeComponent } from "./resume/resume.component";
+import { ContactComponent } from "./contact/contact.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ProjectComponent, data: { animation: "project" } },
+  {
+    path: "projects",
+    component: ProjectComponent,
+    data: { animation: "projects" }
+  },
+  {
+    path: "contact",
+    component: ContactComponent,
+    data: { animation: "contact" }
+  },
+  { path: "resume", component: ResumeComponent, data: { animation: "resume" } }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
